555e2d7c439a91aa1e30e51657974bc78c5479e0
mjhika <1zzt6ovh@mojica.anonaddy.com>
2023-07-10 15:28:16 -0400

    refactor menu
    add mobile menu
    improve design of web menu

---
+++ themes/church-site/static/js/menu-toggle.js
@@ -0,0 +1,8 @@
+function navItemsToggle() {
+  var x = document.getElementById("mobile-menu");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
