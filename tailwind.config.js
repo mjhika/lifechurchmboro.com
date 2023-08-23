257f8e6f51163890b275ca532b2e55d7fdfb8651
mjhika <1zzt6ovh@mojica.anonaddy.com>
2023-08-23 16:25:30 -0400

    integrate postcss and tailwindcss 3
    hugo can integrate tailwind 3 jit with some postcss config. this commit sets up the project config for tailwindcss 3.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
+const typography = require("@tailwindcss/typography");
 
 module.exports = {
-  content: ["./**/*.{html,js,md}", "!./node_modules"],
+  // content: ["./**/*.{html,js,md}", "!./node_modules"],
+  // content: ["./**/*.{js,md}", "!./node_modules", "./hugo_stats.json"],
+  content: ["./hugo_stats.json"],
   darkMode: "class",
   theme: {
     extend: {
@@ -81,5 +84,5 @@ module.exports = {
       display: ["dark"],
     },
   },
-  plugins: [require("tailwindcss-hamburgers")],
+  plugins: [typography],
 };
